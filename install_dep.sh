b40b9b3470cf92007193d364af97a806f9f8731d
Nubonetics <45704452+nubonetics@users.noreply.github.com>
2019-05-04 08:57:18 -0700

    Create install_dep.sh

---
+++ install_dep.sh
@@ -0,0 +1,31 @@
+apt-get update && apt-get install -y \
+    gcc \
+    g++ \
+    git \
+    libeigen3-dev \
+    libgmp-dev \
+    libmpfr-dev \
+    libgmpxx4ldbl \
+    libboost-dev \
+    libboost-thread-dev \
+    libtbb-dev \
+    python3-dev \
+    python3-setuptools \
+	  python3-numpy \
+	  python3-scipy \
+	  python3-nose \
+	  python3-pip \
+    wget && \
+    apt-get clean
+
+mkdir /tmp/cmake
+cd /tmp/cmake
+
+wget https://github.com/Kitware/CMake/releases/download/v3.13.4/cmake-3.13.4.tar.gz && \
+    tar -xzvf cmake-3.13.4.tar.gz > /dev/null
+
+cd cmake-3.13.4
+
+./bootstrap > /dev/null && \
+    make -j$(nproc --all) > /dev/null && \
+    make install > /dev/null
